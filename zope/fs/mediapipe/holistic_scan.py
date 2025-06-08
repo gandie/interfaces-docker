fbe1568aad9274cbc473af23ac41c9397ec7feef
Lars Bergmann <lars@bergmann82.de>
2025-06-08 15:30:21 +0200

    only keep attrs needed

--- zope/fs/mediapipe/holistic_scan.py
+++ zope/fs/mediapipe/holistic_scan.py
@@ -53,12 +53,16 @@ def scan(img_paths):
     hand_key_d = {}
     for key in keys:
         value = getattr(mp_holistic.HandLandmark, key)
+        if not getattr(value, "value", None):
+            continue
         hand_key_d[value.value] = key
 
     keys = [item for item in dir(mp_holistic.PoseLandmark) if not item.startswith('__')]
     body_key_d = {}
     for key in keys:
         value = getattr(mp_holistic.PoseLandmark, key)
+        if not getattr(value, "value", None):
+            continue
         body_key_d[value.value] = key
 
     # ... hack end
